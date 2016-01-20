2ac31d393d3bd2deea812a6dbc75b899b22bb52d
michael lor and nic netzel <netzeln@gmail.com>
2016-01-20 09:32:07 -0800

    set up first spec condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+// var vowelArray = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"];
+var isVowel = function(string){
+ var firstCharacter = string.charAt(0);
+ console.log(firstCharacter);
+if ((firstCharacter === "a") || (firstCharacter === "e") || (firstCharacter === "i") || (firstCharacter === "o") || (firstCharacter === "u") || (firstCharacter === "A") || (firstCharacter === "E") || (firstCharacter === "I") || (firstCharacter === "O") || (firstCharacter === "U")) {
+    return true;
+  } else{
+    return false;
+  }console.log(firstCharacter);
+
+
+}
+
 $(document).ready(function() {
 
 
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe("isVowel", function(){
+  it("checks the value of the first character in the word to see if it's a vowel", function(){
+     expect(isVowel("1ipple")).to.equal(true);
+  });
+});
