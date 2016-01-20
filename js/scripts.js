43605b27f820a2cb4987b8b0d68799c5cb280910
michael lor and nic netzel <michael.vang.1985@gmail.com>
2016-01-20 10:20:53 -0800

    add another spec condition

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,30 @@
 // var vowelArray = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"];
+ var firstCharacter;
+
 var isVowel = function(string){
- var firstCharacter = string.charAt(0);
- console.log(firstCharacter);
+ firstCharacter = string.charAt(0);
+
 if ((firstCharacter === "a") || (firstCharacter === "e") || (firstCharacter === "i") || (firstCharacter === "o") || (firstCharacter === "u") || (firstCharacter === "A") || (firstCharacter === "E") || (firstCharacter === "I") || (firstCharacter === "O") || (firstCharacter === "U")) {
-    return true;
+   return true;
   } else{
     return false;
-  }console.log(firstCharacter);
+  }
+}
 
+var isNotVowel = function(string){
+ firstCharacter = string.charAt(0);
 
+  if ((firstCharacter !== "a") && (firstCharacter !== "e") && (firstCharacter !== "i") && (firstCharacter !== "o") && (firstCharacter !== "u") && (firstCharacter !== "A") && (firstCharacter !== "E") && (firstCharacter !== "I") && (firstCharacter !== "O") && (firstCharacter !== "U")){
+   return true;
+ }else{
+   return false;
+ }
+}
+
+var addAy = function(string) {
+  var theWord = string;
+  theWord = theWord + "ay";
+  return theWord;
 }
 
 $(document).ready(function() {
--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,16 @@
 describe("isVowel", function(){
   it("checks the value of the first character in the word to see if it's a vowel", function(){
-     expect(isVowel("1ipple")).to.equal(true);
+     expect(isVowel("apple")).to.equal(true);
+  });
+});
+
+describe("isNotVowel", function(){
+  it("checks the value of the first character in the word to see if it's not a vowel", function(){
+     expect(isNotVowel("plum")).to.equal(true);
+  });
+});
+describe("addAy", function(){
+  it("adds 'ay' to the end of a word beginning with a vowel", function(){
+    expect(addAy("apple")).to.equal("appleay");
   });
 });
