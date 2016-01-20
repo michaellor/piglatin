e2f87563dba97f03c9a1141c5e2730eab6c63ac8
michael lor and nic netzel <netzeln@gmail.com>
2016-01-20 15:47:08 -0800

    create master function again to store information

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,23 @@
+var pigLatin = function(string) {
 
-   var firstCharacter;
+  if (isVowel = true) {
+    string = string + "ay";
+    return string;
+  } else {
+    return firstConsonants(string);
+  }
+
+}
+
+
 
   var isVowel = function(string){
-   firstCharacter = string.charAt(0);
+  var firstCharacter = string.charAt(0);
 
   if ((firstCharacter === "a") || (firstCharacter === "e") || (firstCharacter === "i") || (firstCharacter === "o") || (firstCharacter === "u") || (firstCharacter === "A") || (firstCharacter === "E") || (firstCharacter === "I") || (firstCharacter === "O") || (firstCharacter === "U")) {
-     return addAy(string);
+     return true;
     } else{
-      return isNotVowel(string);
+      return false;
     }
   }
 
@@ -16,23 +26,39 @@
 
 
     if ((firstCharacter !== "a") && (firstCharacter !== "e") && (firstCharacter !== "i") && (firstCharacter !== "o") && (firstCharacter !== "u") && (firstCharacter !== "A") && (firstCharacter !== "E") && (firstCharacter !== "I") && (firstCharacter !== "O") && (firstCharacter !== "U")){
-     return firstConsonants(string);
+     return firstConsonants(string); //ultimately splice
+
    }else{
      return false;
    }
 
   }
 
+
   var firstConsonants = function(string){
-    var i = 0;
-    for(i, i === , i++)
-    firstCharacter = string.charAt(0);
+    var index = 0;
+
+    for(index ;; index++ ){
+      if ((string.charAt(index) === "u") || (string.charAt(index) === "U") || (string.charAt(index) === "a") || (string.charAt(index) === "A") || (string.charAt(index) === "e") || (string.charAt(index) === "E") || (string.charAt(index) === "i") || (string.charAt(index) === "I") || (string.charAt(index) === "O") || (string.charAt(index) === "o")) {
+
+        console.log(index);
+        return index;
+
+
+      }
+    }
+
+    var sliceFront = string.slice(0,index);
+    string = string.slice(index);
+
+
+    var sliceAy = sliceFront + "ay";
+    alert(sliceFront);
+    console.log(sliceFront);
+
   }
 
-  var addAy = function(string) {
-    string = string + "ay";
-    return string;
-  }
+
 
 
 
--- spec/specs.js
+++ spec/specs.js
@@ -2,24 +2,24 @@
 
 describe("isVowel", function(){
   it("checks the value of the first character in the word to see if it's a vowel", function(){
-     expect(isVowel("apple")).to.equal("appleay");
+     expect(isVowel("apple")).to.equal(true);
   });
 });
-
-describe("isNotVowel", function(){
-  it("checks the value of the first character in the word to see if it's not a vowel", function(){
-     expect(isNotVowel("plum")).to.equal("plumay");
-  });
-});
+//
+// describe("isNotVowel", function(){
+//   it("checks the value of the first character in the word to see if it's not a vowel", function(){
+//      expect(isNotVowel("plum")).to.equal("umplay");
+//   });
+// });
 
 describe("firstConsonants", function(){
   it("counts the number of first consonants in a string", function(){
-     expect(firstConsonants("plum")).to.equal(2);
+     expect(firstConsonants("fruit")).to.equal(2);
   });
 });
 
-describe("addAy", function(){
-  it("adds 'ay' to the end of a word beginning with a vowel", function(){
-    expect(addAy("apple")).to.equal("appleay");
-  });
-});
+// describe("addAy", function(){
+//   it("adds 'ay' to the end of a word beginning with a vowel", function(){
+//     expect(addAy("apple")).to.equal("appleay");
+//   });
+// });
