0c0a3c1c0e9d597419d9f8ce6f66524f0309aa84
michael lor and nic netzel <netzeln@gmail.com>
2016-01-20 11:42:00 -0800

    add in functionality for determing vowel or consonants.

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,43 @@
-// var vowelArray = ["A", "E", "I", "O", "U", "a", "e", "i", "o", "u"];
- var firstCharacter;
 
-var isVowel = function(string){
- firstCharacter = string.charAt(0);
+   var firstCharacter;
 
-if ((firstCharacter === "a") || (firstCharacter === "e") || (firstCharacter === "i") || (firstCharacter === "o") || (firstCharacter === "u") || (firstCharacter === "A") || (firstCharacter === "E") || (firstCharacter === "I") || (firstCharacter === "O") || (firstCharacter === "U")) {
-   return true;
-  } else{
-    return false;
+  var isVowel = function(string){
+   firstCharacter = string.charAt(0);
+
+  if ((firstCharacter === "a") || (firstCharacter === "e") || (firstCharacter === "i") || (firstCharacter === "o") || (firstCharacter === "u") || (firstCharacter === "A") || (firstCharacter === "E") || (firstCharacter === "I") || (firstCharacter === "O") || (firstCharacter === "U")) {
+     return addAy(string);
+    } else{
+      return isNotVowel(string);
+    }
   }
-}
 
-var isNotVowel = function(string){
- firstCharacter = string.charAt(0);
-
-  if ((firstCharacter !== "a") && (firstCharacter !== "e") && (firstCharacter !== "i") && (firstCharacter !== "o") && (firstCharacter !== "u") && (firstCharacter !== "A") && (firstCharacter !== "E") && (firstCharacter !== "I") && (firstCharacter !== "O") && (firstCharacter !== "U")){
-   return true;
- }else{
-   return false;
- }
-}
-
-var addAy = function(string) {
-  var theWord = string;
-  theWord = theWord + "ay";
-  return theWord;
-}
-
-$(document).ready(function() {
+  var isNotVowel = function(string){
+   firstCharacter = string.charAt(0);
 
 
+    if ((firstCharacter !== "a") && (firstCharacter !== "e") && (firstCharacter !== "i") && (firstCharacter !== "o") && (firstCharacter !== "u") && (firstCharacter !== "A") && (firstCharacter !== "E") && (firstCharacter !== "I") && (firstCharacter !== "O") && (firstCharacter !== "U")){
+     return firstConsonants(string);
+   }else{
+     return false;
+   }
 
-});
+  }
+
+  var firstConsonants = function(string){
+    var i = 0;
+    for(i, i === , i++)
+    firstCharacter = string.charAt(0);
+  }
+
+  var addAy = function(string) {
+    string = string + "ay";
+    return string;
+  }
+
+
+
+  $(document).ready(function() {
+
+
+
+          });
--- spec/specs.js
+++ spec/specs.js
@@ -1,14 +1,23 @@
+
+
 describe("isVowel", function(){
   it("checks the value of the first character in the word to see if it's a vowel", function(){
-     expect(isVowel("apple")).to.equal(true);
+     expect(isVowel("apple")).to.equal("appleay");
   });
 });
 
 describe("isNotVowel", function(){
   it("checks the value of the first character in the word to see if it's not a vowel", function(){
-     expect(isNotVowel("plum")).to.equal(true);
+     expect(isNotVowel("plum")).to.equal("plumay");
   });
 });
+
+describe("firstConsonants", function(){
+  it("counts the number of first consonants in a string", function(){
+     expect(firstConsonants("plum")).to.equal(2);
+  });
+});
+
 describe("addAy", function(){
   it("adds 'ay' to the end of a word beginning with a vowel", function(){
     expect(addAy("apple")).to.equal("appleay");
